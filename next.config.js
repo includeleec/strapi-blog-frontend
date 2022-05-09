d78775b7c5b09c317eef44b9b07e6cc071b0768a
includeleec <includeleec@gmail.com>
2022-05-10 00:25:15 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  images: {
+    loader: "default",
+    domains: ["nrm86mk7shfsr31tui1ckn3qbc.akash-la-1.maxihost.com"],
+  },
+}
+
+module.exports = nextConfig
